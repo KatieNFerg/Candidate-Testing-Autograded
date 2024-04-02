3b34baaf118a9168c886244eaece1e594cc400b2
KatieNFerg <ketchersidekatie@gmail.com>
2024-04-02 15:28:05 -0500

    finished part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,8 +11,8 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5+3)/2*10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45] what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
-let correctAnswers = ["Sally Ride ", "true ", "40 ", "Trajectory ", 3];
+let questions = ["Who was the first American woman in space? " , "True or false: 5 kilometer == 5000 meters? " , "(5 + 3)/2 * 10 = ? " , "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? " , "What is the minimum crew size for the ISS? " ];
+let correctAnswers = ["Sally Ride" , "true" , "40" , "Trajectory" , "3"];
 let candidateAnswers = [];
 
 
@@ -30,20 +30,21 @@ function askQuestion() {
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  for (i = 0; i < questions.length; i++) {
-    console.log(`${questions[i]} Your Answer: ${candidateAnswers[i]}\n Correct Answer: ${correctAnswers[i]}`);
+  for (i = 0; i < candidateAnswers.length; i++) {
+    console.log(`${i+1} ${questions[i]} Your Answer: ${candidateAnswers[i]}\n Correct Answer: ${correctAnswers[i]}`);
     if (String(candidateAnswers[i]).toLowerCase() === String(correctAnswers[i]).toLowerCase()) {
     }
   }
 
 
   let grade = 0;  //TODO 3.2 use this variable to calculate the candidates score.
-  if (candidateAnswers == correctAnswers) {
-    candidateAnswers = true;
-  }
+ 
   for (let i = 0; i < questions.length; i++) {
     grade = 0;
-    grade = (candidateAnswers[i] / questions) * 100
+    grade = (candidateAnswers[i] / questions[i]) * 100
+    if (candidateAnswers === correctAnswers) {
+      candidateAnswers = true;
+    }
     if (grade >= 80) {
       console.log("You passed the quiz! ");
     } else {
